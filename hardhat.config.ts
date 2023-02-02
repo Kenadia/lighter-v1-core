f35e34d9b80940bcc08148a7eac479670304762f
Kenadia <Kenadia@users.noreply.github.com>
2023-02-02 09:27:45 -0500

    Add forked network support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,28 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
+import { HardhatUserConfig } from "hardhat/config";
+import { HardhatNetworkUserConfig, HttpNetworkUserConfig } from "hardhat/types";
+
+// Throw-away key.
+const ALCHEMY_KEY = 'asKzBAStzKF-PeLfUIXc2zpj0kf7vM1a'
+
+function getHardhatConfig() {
+  const networkConfig: HardhatNetworkUserConfig = {
+    allowUnlimitedContractSize: true,
+  };
+
+  // Fork ETH mainnet.
+  if (process.env.FORK) {
+    networkConfig.chainId = 1;
+    networkConfig.forking = {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+    };
+    (networkConfig as HttpNetworkUserConfig).timeout = 0;
+  }
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.16",
-};
-
-export default config;
-
-module.exports = {
   solidity: {
     version: "0.8.16",
     settings: {
@@ -20,8 +33,8 @@ module.exports = {
     },
   },
   networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true,
-    },
+    hardhat: getHardhatConfig(),
   },
 };
+
+export default config;
